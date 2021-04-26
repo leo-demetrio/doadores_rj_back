15efcfc474f3b382580e7db4eeafc95b76f148a2
leo-demetrio <leocdemetrio@gmail.com>
2021-04-26 17:49:19 -0300

    change port

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const { check } = require('express-validator');
 require('./database/conexao');
 
 const app = express();
-const port = process.env.Port || 3000;
+const port = process.env.PORT || 3000;
 app.use(cors());
 app.use((req,res,next) => {
     res.header("Access-Control-Allow-Origin","*");
