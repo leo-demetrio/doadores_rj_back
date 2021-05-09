e2ad2457fc5bae5c3d1da5915d203e5072b6f48a
leo-demetrio <leocdemetrio@gmail.com>
2021-05-09 12:33:49 -0300

    add configuration heroku database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,10 @@
 console.log('.env',process.env.DATABASE_URL)
 module.exports = {
-    type: 'postgres',
+    user: "uicpyntyelhrdm",
+    password: "c24027f8c4b599452b78df3fcff171fc851ba179e0703ce17d428d6a7b192f55",
+    database: "d76gff4q0tb1bn",
+    port: 5432,
+    host: "ec2-34-201-248-246.compute-1.amazonaws.com",
     dialect: 'postgres',
     url: process.env.DATABASE_URL,
     ssl: true,
