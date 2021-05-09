d58839afbf53c4283fa115c87ee6146832ceac67
leo-demetrio <leocdemetrio@gmail.com>
2021-05-09 13:34:48 -0300

    change

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,6 @@
 console.log('.env',process.env.DATABASE_URL)
 module.exports = {
+    "use_env_variable": "DATABASE_URL",
     user: "uicpyntyelhrdm",
     password: "c24027f8c4b599452b78df3fcff171fc851ba179e0703ce17d428d6a7b192f55",
     database: "d76gff4q0tb1bn",
