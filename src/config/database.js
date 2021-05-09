0b8f9e72775519dc27fc65aca3ca30241f04a4ed
leo-demetrio <leocdemetrio@gmail.com>
2021-05-09 19:38:40 -0300

    teste bd

--- src/config/database.js
+++ src/config/database.js
@@ -2,11 +2,11 @@ console.log('.env',process.env.DATABASE_URL)
 module.exports = {
     production: {
         dialect: 'postgres',
+        url: process.env.DATABASE_URL,
         username: 'uicpyntyelhrdm',
         password: 'c24027f8c4b599452b78df3fcff171fc851ba179e0703ce17d428d6a7b192f55',
         database: 'd76gff4q0tb1bn',
         host: 'ec2-34-201-248-246.compute-1.amazonaws.com',
-        dialect: 'postgres'
        },
     "dialect": 'postgres',
     "type": 'postgres',
