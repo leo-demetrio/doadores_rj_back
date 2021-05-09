af57e9b4fe5543cd57bcc6738715ed74a0bf2d7f
leo-demetrio <leocdemetrio@gmail.com>
2021-05-09 19:57:39 -0300

    teste bd

--- src/config/database.js
+++ src/config/database.js
@@ -7,6 +7,7 @@ module.exports = {
         password: 'c24027f8c4b599452b78df3fcff171fc851ba179e0703ce17d428d6a7b192f55',
         database: 'd76gff4q0tb1bn',
         host: 'ec2-34-201-248-246.compute-1.amazonaws.com',
+        ssl: true
        },
     "dialect": 'postgres',
     "type": 'postgres',
