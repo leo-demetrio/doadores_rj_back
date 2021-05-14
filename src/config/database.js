3ee080226e3086213fa1027d1539fb0aaabab1c0
leo-demetrio <leocdemetrio@gmail.com>
2021-05-14 14:46:05 -0300

    configuration bd local

--- src/config/database.js
+++ src/config/database.js
@@ -1,18 +1,34 @@
 console.log('.env',process.env.DATABASE_URL)
 module.exports = {
-    production: {
         dialect: 'postgres',
-        url: process.env.DATABASE_URL,
-        username: 'uicpyntyelhrdm',
-        password: 'c24027f8c4b599452b78df3fcff171fc851ba179e0703ce17d428d6a7b192f55',
-        database: 'd76gff4q0tb1bn',
-        host: 'ec2-34-201-248-246.compute-1.amazonaws.com',
-        ssl: true
-       },
-    "dialect": 'postgres',
-    "type": 'postgres',
-    "url": process.env.DATABASE_URL,   
-}
+        host: 'localhost',
+        username: 'postgres',
+        // password: 'leoppostegres',
+        password: 'leoppostegres',
+        database: process.env.NODE_ENV === "test" ? "syscondoe_test" : 'doadores_rj',
+        define: {
+            timestamps: true,
+            underscored: true,
+        }
+    
+    }
+
+
+// module.exports = {
+//     production: {
+//         dialect: 'postgres',
+//         url: process.env.DATABASE_URL,
+//         username: 'uicpyntyelhrdm',
+//         password: 'c24027f8c4b599452b78df3fcff171fc851ba179e0703ce17d428d6a7b192f55',
+//         database: 'd76gff4q0tb1bn',
+//         host: 'ec2-34-201-248-246.compute-1.amazonaws.com',
+//         ssl: true
+//        },
+//     "dialect": 'postgres',
+//     "type": 'postgres',
+
+//     "url": process.env.DATABASE_URL,   
+// }
 // module.exports = {
 //     user: "uicpyntyelhrdm",
 //     password: "c24027f8c4b599452b78df3fcff171fc851ba179e0703ce17d428d6a7b192f55",
