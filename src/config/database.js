0d51c62136cf5e5936c8cc8f507bfc5cd1c8adb3
leo-demetrio <leocdemetrio@gmail.com>
2021-06-01 17:19:21 -0300

    tutorial heroku

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,9 @@
 console.log('.env',process.env.DATABASE_URL)
 
 module.exports = {
-    dialect: 'postgres',
-    url: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL,
     ssl: {
-        "rejectUnauthorized": false
+        rejectUnauthorized: false
     }
 }
 // module.exports = {
